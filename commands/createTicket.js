84fa0b180fa0619d6d855b15b6c6b5c69fda27ed
Austin <HunterKillerDrone154@gmail.com>
2019-03-10 10:12:04 -0400

    Create createTicket.js

---
+++ commands/createTicket.js
@@ -0,0 +1,29 @@
+module.exports.run = (bot, message, args, Discord) => {
+let num = Math.floor(Math.random() * 9999);
+//RNG
+let em = new Discord.RichEmbed()
+.setTitle("Ticket Created!")
+.setDescription("Staff will anwser as soon as they can!")
+.addField("Your ticket number is:", num)
+
+ message.guild.createChannel(`ticket-${num}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Staff");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+bot.message.channel.send({embed: em})
+}
+module.exports.help = {
+"name": "new"
+}
