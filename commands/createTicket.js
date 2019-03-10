5a946ce5c227bd6ebf0784f6c3606b83746b038e
Austin <HunterKillerDrone154@gmail.com>
2019-03-10 10:19:27 -0400

    Update createTicket.js

--- commands/createTicket.js
+++ commands/createTicket.js
@@ -1,10 +1,20 @@
 module.exports.run = (bot, message, args, Discord) => {
 let num = Math.floor(Math.random() * 9999);
+let tr = args.join(" ")
 //RNG
 let em = new Discord.RichEmbed()
 .setTitle("Ticket Created!")
 .setDescription("Staff will anwser as soon as they can!")
 .addField("Your ticket number is:", num)
+.setColor("GREEN")
+.setFooter(`Requested by: ${message.author.username}`)
+
+let emb = new Discord.RichEmbed()
+.setTitle("Hello! Our staff will be with you shortly!")
+.setDescription("If this is a order feel free to ping Negative or Geek Squad :D")
+.addField("The reason for this ticket is: ${tr}")
+.setColor("GREEN")
+.setFooter(`Requested by: ${message.author.username}`)
 
  message.guild.createChannel(`ticket-${num}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Ticket Access");
@@ -23,7 +33,7 @@ let em = new Discord.RichEmbed()
             });
  });
 message.channel.send({embed: em})
-  
+message.guild.channels.find("name", `ticket-${num}`).send({embed{emb}})
 }
 module.exports.help = {
 "name": "new"
